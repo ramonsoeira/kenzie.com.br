75afa4279222d3af84297938fb4ff5aa596ad9e4
Luigi Domingues <luigidomingues@gmail.com>
2019-12-17 20:47:02 -0300

    chore: add Sentry SDK and remove hotjar

--- src/components/seo.js
+++ src/components/seo.js
@@ -97,19 +97,6 @@ function SEO({ description, lang, meta, title }) {
         }
       ].concat(meta)}
     >
-      <script>
-        {process.env.NODE_ENV !== "development" &&
-          `
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:1543038,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </script>
 
       <script
         src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
--- src/layout/default.js
+++ src/layout/default.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import SEO from "@components/seo";
 import WhatsappButton from "@components/whatsapp-button";
 import LeadForm from "@components/lead-form";
@@ -7,8 +6,20 @@ import Topbar from "@components/top-bar";
 import Header from "@components/header";
 import CTABar from "@components/cta-bar";
 import Footer from "@components/footer";
-
 import "@styles/index.scss";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    dsn: "https://73f769c3630d445398f46ddd664d2fe3@sentry.io/1860386",
+    integrations: [
+      new Integrations.CaptureConsole({
+        levels: ["error"]
+      })
+    ]
+  });
+}
 
 export default ({ children, title, description, searchParams, ...rest }) => {
   const [openLeadForm, setOpenLeadForm] = useState(false);
