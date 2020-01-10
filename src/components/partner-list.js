61817f4ef81eac81f1e2a19df5b795a6335d1bc9
Luigi Domingues <luigidomingues@gmail.com>
2020-01-10 00:16:58 -0300

    chore: add VHSYS partner

--- src/components/partner-list.js
+++ src/components/partner-list.js
@@ -6,44 +6,79 @@ import EBANX from "@images/partners/ebanx.png";
 import GrupoBarigui from "@images/partners/grupo-barigui.png";
 import James from "@images/partners/james.png";
 import SocialWave from "@images/partners/social-wave.png";
+import VHSYS from "@images/partners/vhsys.png";
 
 const PartnerList = () => {
   return (
     <div className="container">
       <div className="columns is-mobile is-multiline is-vcentered">
         <div className="column is-half-mobile has-text-centered">
-          <a href="https://www.bancobari.com.br/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.bancobari.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img width="120px" src={BancoBari} alt="Banco Bari" />
           </a>
         </div>
 
         <div className="column is-half-mobile has-text-centered">
-          <a href="https://www.connectplug.com.br/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.connectplug.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img width="120px" src={CPlug} alt="CPlug" />
           </a>
         </div>
 
         <div className="column is-half-mobile has-text-centered">
-          <a href="https://www.jamesdelivery.com.br/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.jamesdelivery.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img width="120px" src={James} alt="James" />
           </a>
         </div>
 
         <div className="column is-half-mobile has-text-centered">
-          <a href="https://www.grupobarigui.com.br/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.grupobarigui.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img width="120px" src={GrupoBarigui} alt="Grupo Barigui" />
           </a>
         </div>
 
         <div className="column is-half-mobile has-text-centered">
-          <a href="https://www.ebanx.com/br/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.ebanx.com/br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img width="120px" src={EBANX} alt="EBANX" />
           </a>
         </div>
 
         <div className="column is-half-mobile has-text-centered">
-          <a href="https://socialwave.com.br/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://socialwave.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img width="120px" src={SocialWave} alt="Social Wave" />
+          </a>
+        </div>
+
+        <div className="column is-half-mobile has-text-centered">
+          <a
+            href="https://vhsys.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img width="120px" src={VHSYS} alt="VHSYS" />
           </a>
         </div>
       </div>
