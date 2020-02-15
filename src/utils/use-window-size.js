dd8d9ff7ba4ee8c6f50affc9f0277f2c03561cd2
Luigi Domingues <luigidomingues@gmail.com>
2020-02-15 00:42:54 -0300

    chore: remove use window size

--- src/utils/use-window-size.js
+++ src/utils/use-window-size.js
@@ -1,28 +1,28 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const useWindowSize = () => {
-  const isClient = typeof window === "object";
+// const useWindowSize = () => {
+//   const isClient = typeof window === "object";
 
-  function getSize() {
-    return isClient ? window.innerWidth : undefined;
-  }
+//   function getSize() {
+//     return isClient ? window.innerWidth : undefined;
+//   }
 
-  const [windowSize, setWindowSize] = useState(getSize);
+//   const [windowSize, setWindowSize] = useState(getSize);
 
-  useEffect(() => {
-    if (!isClient) {
-      return false;
-    }
+//   useEffect(() => {
+//     if (!isClient) {
+//       return false;
+//     }
 
-    function handleResize() {
-      setWindowSize(getSize());
-    }
+//     function handleResize() {
+//       setWindowSize(getSize());
+//     }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []); // Empty array ensures that effect is only run on mount and unmount
 
-  return windowSize;
-};
+//   return windowSize;
+// };
 
-export default useWindowSize;
+// export default useWindowSize;
