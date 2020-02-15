a70d10d497d1236b2d4b58d24dcf233b2bbfc96a
Luigi Domingues <luigidomingues@gmail.com>
2020-02-15 00:46:58 -0300

    chore: remove header component

--- src/layout/default.js
+++ src/layout/default.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { BrowserView, MobileView } from "react-device-detect";
 
 import SEO from "@components/seo";
 import WhatsappButton from "@components/whatsapp-button";
 import LeadForm from "@components/lead-form";
-import Header from "@components/header";
 import HeaderMobile from "@components/header-mobile";
 import CTABar from "@components/cta-bar";
 import Footer from "@components/footer";
@@ -24,13 +22,7 @@ export default ({ children, title, description, ...rest }) => {
 
       <LeadForm isOpen={openLeadForm} onClose={() => setOpenLeadForm(false)} />
 
-      <BrowserView>
-        <Header {...headerProps} />
-      </BrowserView>
-
-      <MobileView>
-        <HeaderMobile {...headerProps} />
-      </MobileView>
+      <HeaderMobile {...headerProps} />
 
       {children}
 
