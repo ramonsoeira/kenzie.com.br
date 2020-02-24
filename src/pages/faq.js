7041808403c2a00f2263154af0ada10e42ffe023
Luigi Domingues <luigidomingues@gmail.com>
2020-02-24 12:46:51 -0300

    chore: fix padding/margin to FAQ page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -17,35 +17,33 @@ const FAQ = props => {
   return (
     <React.Fragment>
       <Layout className="kz-faq-page" title="FAQ | Kenzie Academy Brazil">
-        <div className="kz-section">
-          <div className="container">
-            <div className="columns">
-              <div className="column">
-                <div className="kz-section-head">
-                  <h3 className="kz-section-title">Perguntas frequentes</h3>
-                </div>
-
-                {FAQList.map(({ title, text }, index) => {
-                  return (
-                    <Accordion
-                      allowMultipleExpanded={true}
-                      allowZeroExpanded={true}
-                      key={index}
-                    >
-                      <AccordionItem>
-                        <AccordionItemHeading>
-                          <AccordionItemButton>{title}</AccordionItemButton>
-                        </AccordionItemHeading>
-
-                        <AccordionItemPanel>
-                          <p dangerouslySetInnerHTML={{ __html: text }} />
-                        </AccordionItemPanel>
-                      </AccordionItem>
-                    </Accordion>
-                  );
-                })}
+        <div class="kz-section">
+          <div class="container">
+            <div class="columns">
+              <div class="column has-text-centered">
+                <h3 class="kz-section-head-title">Perguntas frequentes</h3>
               </div>
             </div>
+
+            {FAQList.map(({ title, text }, index) => {
+              return (
+                <Accordion
+                  allowMultipleExpanded={true}
+                  allowZeroExpanded={true}
+                  key={index}
+                >
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>{title}</AccordionItemButton>
+                    </AccordionItemHeading>
+
+                    <AccordionItemPanel>
+                      <p dangerouslySetInnerHTML={{ __html: text }} />
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                </Accordion>
+              );
+            })}
           </div>
         </div>
       </Layout>
