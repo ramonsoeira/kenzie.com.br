f23b35e312b697f2bee8aa5fbcefe607ee1771be
Luigi Domingues <luigidomingues@gmail.com>
2020-03-10 22:37:08 -0300

    chore: add school section to production home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import { graphql } from "gatsby";
 import { Link } from "gatsby";
 import Layout from "@layout/default";
+import Fade from "react-reveal/Fade";
 
 //Components
+import Box from "@components/box";
 import Card from "@components/card";
 import Thumbnail from "@components/thumbnail";
 import PartnerList from "@components/partner-list";
 import NewsList from "@components/news-list";
+
+//Images
+import BoyImage from "@images/illustrates/boy.png";
+import GirlImage from "@images/illustrates/girl.png";
 
 //Icon
 import TechHandsIcon from "@icons/tech-hands";
@@ -27,7 +33,9 @@ const Index = props => {
           <div className="container">
             <div className="columns is-vcentered reverse-columns-tablet">
               <div className="column">
-                <h1 className="kz-hero-title">Comece aqui sua carreira</h1>
+                <Fade top>
+                  <h1 className="kz-hero-title">Comece aqui sua carreira</h1>
+                </Fade>
 
                 <div className="kz-hero-content">
                   {/* prettier-ignore */}
@@ -67,74 +75,79 @@ const Index = props => {
         <div className="kz-section with-blue-bg">
           <div className="container">
             <div className="kz-section-head">
-              <h3 className="kz-section-featured-title">
-                Curso de programação
-              </h3>
+              <Fade top>
+                <h3 className="kz-section-featured-title">
+                  Curso de programação
+                </h3>
+              </Fade>
+
               <p className="kz-section-subtitle">
                 Mais de 2.000 mil horas de curso
               </p>
             </div>
 
-            <div className="columns is-justified-between">
-              <div className="column">
-                <Card
-                  icon={<TechHandsIcon />}
-                  title="O que você vai aprender aqui?"
-                  buttonLabel="Saiba mais"
-                  buttonTo="/curriculo"
-                  align="left"
-                >
-                  Linguagens de programação modernas e muito utilizadas no
-                  mercado. <br />
-                  <span className="is-bold">Front-end:</span> HTML, CSS,
-                  JavaScript e React. <br />
-                  <span className="is-bold">Back-end:</span> Python e Node.jS.
-                </Card>
-              </div>
+            <Fade left>
+              <div className="columns is-justified-between">
+                <div className="column">
+                  <Card
+                    icon={<TechHandsIcon />}
+                    title="O que você vai aprender aqui?"
+                    buttonLabel="Saiba mais"
+                    buttonTo="/curriculo"
+                    align="left"
+                  >
+                    Linguagens de programação modernas e muito utilizadas no
+                    mercado. <br />
+                    <span className="is-bold">Front-end:</span> HTML, CSS,
+                    JavaScript e React. <br />
+                    <span className="is-bold">Back-end:</span> Python e Node.jS.
+                  </Card>
+                </div>
 
-              <div className="column">
-                <Card
-                  icon={<ComputerWithToolsIcon />}
-                  title="Aprenda na prática"
-                  buttonLabel="Saiba mais"
-                  buttonTo="/curriculo"
-                >
-                  Nossos alunos criam um amplo portfólio de projetos realizados,
-                  se preparando para o dia a dia de um programador e facilitando
-                  sua entrada no mercado de trabalho.
-                </Card>
-              </div>
+                <div className="column">
+                  <Card
+                    icon={<ComputerWithToolsIcon />}
+                    title="Aprenda na prática"
+                    buttonLabel="Saiba mais"
+                    buttonTo="/curriculo"
+                  >
+                    Nossos alunos criam um amplo portfólio de projetos
+                    realizados, se preparando para o dia a dia de um programador
+                    e facilitando sua entrada no mercado de trabalho.
+                  </Card>
+                </div>
 
-              <div className="column">
-                <Card
-                  icon={<MethodologyIcon />}
-                  title="Metodologia comprovada"
-                  buttonLabel="Saiba mais"
-                  buttonTo="/curriculo"
-                  align="right"
-                >
-                  A combinação de aulas, ensino baseado em projetos,
-                  acompanhamento do nosso time e currículo validado nos Estados
-                  Unidos vai acelerar sua carreira para o sucesso.
-                </Card>
+                <div className="column">
+                  <Card
+                    icon={<MethodologyIcon />}
+                    title="Metodologia comprovada"
+                    buttonLabel="Saiba mais"
+                    buttonTo="/curriculo"
+                    align="right"
+                  >
+                    A combinação de aulas, ensino baseado em projetos,
+                    acompanhamento do nosso time e currículo validado nos
+                    Estados Unidos vai acelerar sua carreira para o sucesso.
+                  </Card>
+                </div>
               </div>
-            </div>
+            </Fade>
           </div>
         </div>
 
         <div className="kz-section">
-          <div className="container">
-            <div className="columns">
-              <div className="column">
-                <div className="kz-section-head">
+          <div className="columns">
+            <div className="column">
+              <div className="kz-section-head">
+                <Fade top>
                   <h3 className="kz-section-featured-title">
                     Conheça os nossos parceiros que podem dar início a sua
                     carreira de sucesso.
                   </h3>
-                </div>
-
-                <PartnerList />
+                </Fade>
               </div>
+
+              <PartnerList />
             </div>
           </div>
         </div>
@@ -143,9 +156,11 @@ const Index = props => {
           <div className="container">
             <div className="columns reverse-columns-tablet is-vcentered">
               <div className="column">
-                <h3 className="kz-section-title">
-                  Nós acreditamos em seu potencial!
-                </h3>
+                <Fade top>
+                  <h3 className="kz-section-title">
+                    Nós acreditamos em seu potencial!
+                  </h3>
+                </Fade>
 
                 <div className="kz-hero-content">
                   {/* prettier-ignore */}
@@ -176,12 +191,64 @@ const Index = props => {
           </div>
         </div>
 
-        <div className="kz-section">
+        <div className="kz-section" id="como-funciona-nossa-escola">
           <div className="container">
             <div className="columns">
               <div className="column">
                 <div className="kz-section-head">
-                  <h3 className="kz-section-featured-title">#kenzienamídia</h3>
+                  <Fade top>
+                    <h3 className="kz-section-featured-title">
+                      Como funciona nossa escola?
+                    </h3>
+                  </Fade>
+
+                  <p className="kz-section-subtitle">
+                    Agora iniciamos nosso trabalho no{" "}
+                    <span className="is-bold">Brasil</span>, trazendo todo a
+                    experiência adquirida no mercado americano. Nosso objetivo é
+                    fomentar essa área, garantindo que nossos alunos tenham a
+                    expertise para atuação como{" "}
+                    <span className="is-bold">desenvolvedor full-stack</span>{" "}
+                    após <span className="is-bold">12 meses de curso</span>.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <Fade left>
+              <div className="columns">
+                <div className="column">
+                  <Box
+                    title="Aulas ao vivo"
+                    image={BoyImage}
+                    buttonLabel="Saiba mais"
+                    buttonTo="/aulas-ao-vivo"
+                  />
+                </div>
+
+                <div className="column">
+                  <Box
+                    title="Aulas presenciais"
+                    image={GirlImage}
+                    buttonLabel="Saiba mais"
+                    buttonTo="/aulas-presenciais"
+                  />
+                </div>
+              </div>
+            </Fade>
+          </div>
+        </div>
+
+        <div className="kz-section">
+          <div className="kz-container">
+            <div className="columns">
+              <div className="column">
+                <div className="kz-section-head">
+                  <Fade top>
+                    <h3 className="kz-section-featured-title">
+                      #kenzienamídia
+                    </h3>
+                  </Fade>
                 </div>
 
                 <NewsList />
