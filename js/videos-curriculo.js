7785905e439631533626e8da9a2804571e123194
Joao Pacheco <jao.pacheco1@gmail.com>
2020-10-28 17:03:42 -0300

    fix performance videos curriculo page #2

---
+++ js/videos-curriculo.js
@@ -0,0 +1,29 @@
+$('.video-module1-desktop').one('click', function () {
+
+    var $this = $(this);
+
+    $this.html('<iframe src="https://www.youtube.com/embed/oCvfpbr_Leo"></iframe>');
+});
+
+
+$('.video-module2-desktop').one('click', function () {
+
+    var $this = $(this);
+
+    $this.html('<iframe src="https://www.youtube.com/embed/8yVstZSGcIA"></iframe>');
+});
+
+$('.video-module1-mobile').one('click', function () {
+
+    var $this = $(this);
+
+    $this.html('<iframe src="https://www.youtube.com/embed/oCvfpbr_Leo"></iframe>');
+});
+
+
+$('.video-module2-mobile').one('click', function () {
+
+    var $this = $(this);
+
+    $this.html('<iframe src="https://www.youtube.com/embed/8yVstZSGcIA"></iframe>');
+});
